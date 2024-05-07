173b5a1e62d6df2d71d956521606fd32e96c6d1b
SagiriKonno <137740372+sksk000@users.noreply.github.com>
2024-05-07 14:28:45 +0000

    [update]index.htmlにnew.htmlを表示するように修正、新規追加時にPostでデータを送信できていなかった為Postで送るように修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to:'homes#top'
   resources :books
+  post 'books' => 'books#create'
   patch 'books/:id' => 'books#update', as: 'update_book'
   get 'books/:id' => 'books#show', as: 'show_book'
   delete 'books/:id' => 'books#destroy', as: 'destroy_book'
