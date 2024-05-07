0f3b3d12d69c05f9e094ef74fe4ad38f6a0595e9
SagiriKonno <137740372+sksk000@users.noreply.github.com>
2024-05-07 12:47:31 +0000

    [update]削除機能作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,6 +6,7 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_param)
     book.save
+    # 新規作成後一覧に戻る
     redirect_to books_path
   end
 
@@ -24,7 +25,15 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_param)
+    # 更新後は更新したidのデータを表示する
     redirect_to show_book_url(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    # 削除後は一覧に戻る
+    redirect_to books_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :books
   patch 'books/:id' => 'books#update', as: 'update_book'
   get 'books/:id' => 'books#show', as: 'show_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
